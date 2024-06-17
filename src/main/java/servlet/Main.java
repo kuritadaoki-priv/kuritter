9c754b1312db355e718877bec1f80bb516c76337
kuritadaoki-priv <zhixingjiuli62@gmail.com>
2024-06-18 01:51:52 +0900

    add Posting and Reading

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 @WebServlet("/Main")
@@ -36,11 +37,40 @@ public class Main extends HttpServlet {
     
     if (loginUser == null) { // ログインしていない場合
       // リダイレクト
-      response.sendRedirect("index.jsp");
+      response.sendRedirect("/docoTsubu/");
     } else { // ログイン済みの場合
       // フォワード
-      RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+      RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
       dispatcher.forward(request, response);
     }
+  }
+  
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // リクエストパラメータの取得
+    request.setCharacterEncoding("UTF-8");
+    String text = request.getParameter("text");
+
+    // 入力値チェック
+    if (text != null && text.length() != 0) {
+      // アプリケーションスコープに保存されたつぶやきリストを取得
+      ServletContext application = this.getServletContext();
+      List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+      // セッションスコープに保存されたユーザー情報を取得
+      HttpSession session = request.getSession();
+      User loginUser = (User) session.getAttribute("loginUser");
+
+      // つぶやきをつぶやきリストに追加
+      Mutter mutter = new Mutter(loginUser.getName(), text);
+      PostMutterLogic postMutterLogic = new PostMutterLogic();
+      postMutterLogic.execute(mutter, mutterList);
+
+      // アプリケーションスコープにつぶやきリストを保存
+      application.setAttribute("mutterList", mutterList);
+    }
+
+    // メイン画面にフォワード
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+    dispatcher.forward(request, response);
   }
 }
